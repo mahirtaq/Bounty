048cd6e1e1a41ef43d7eda05bda3f8d9316334b1
Mahir Taqbeem <mahirtaq@umich.edu>
2019-03-04 21:37:58 -0500

    Graphql schema first iteration

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+const graphql = require('graphql');
 
 const app = express();
 
 // Body Parser
 app.use(express.json());
-app.use(espress.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+app.use('/graphql', graphqlHttp({
+    schema: graphql.buildSchema(`
+        type RootQuery {
+            bounties: [String!]!
+        }
+
+        type RootMutation {
+            createBounty(poster: String!, value: Int!, 
+                class: String!, description: String!): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        bounties: () => {
+            return ['OHDd', 'dfadsf', 'adfd'];
+        },
+        createBounty: (args) => {
+            return args.poster;
+        }
+    },
+    graphiql: true
+}));
 
 const PORT = process.env.PORT || 8000;
 
